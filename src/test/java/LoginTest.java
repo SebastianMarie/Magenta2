2cb398bd6100958751d3a64f3bcf00796afcc42e
SebastianMarie <sebastian.rgs@hotmail.com>
2021-01-28 21:23:59 +0200

    Initial commit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
-    public static void main(String[] args) {
+    public void validlogin() {
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://testfasttrackit.info/selenium-test");
